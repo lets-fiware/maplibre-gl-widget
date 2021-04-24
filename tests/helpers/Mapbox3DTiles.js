71e4df8bf8be355fb19eebcfe8aa944ebb3d558e
Kazuhito Suda <kazuhito@fisuda.jp>
2021-04-25 08:53:51 +0900

    Prepare unit test

---
+++ tests/helpers/Mapbox3DTiles.js
@@ -0,0 +1,7 @@
+(function () {
+    "use strict";
+
+    window.Mapbox3DTiles = {
+        Layer: function () {}
+    };
+})();
---
+++ tests/helpers/turf.js
@@ -0,0 +1,10 @@
+(function () {
+    "use strict";
+
+    window.turf = {
+        center: function () {},
+        envelope: function () {},
+        polygon: function () {},
+        booleanIntersects: function () {}
+    };
+})();
